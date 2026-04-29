699797a74f5445afca601d1dd244bcf121ae9407
Onuki <onuki@mist.i.u-tokyo.ac.jp>
2026-04-29 20:58:12 +0900

    StrongApproximation

--- quaternion.py
+++ quaternion.py
@@ -1,5 +1,8 @@
 from sage.all import (
     ZZ,
+    GF,
+    log,
+    kronecker,
     ceil,
     floor,
     gcd,
@@ -12,6 +15,7 @@ from sage.all import (
     matrix,
 )
 from sage.rings.factorint import factor_trial_division
+from sage.modules.free_module_integer import IntegerLattice
 
 def SumOf2Squares(n):
     if n < 0:
@@ -63,6 +67,37 @@ def IdealModConstraint(O0, qj, qk, gamma, alpha, N):
         raise ValueError("IdealModConstraint: no solution")
     assert (gamma * (C*qj + D*qk)) in O0.left_ideal([alpha, N])
     return C, D
+
+# Return a quaternion nu such that nu = Cj + Dk mod N and Nrd(nu) = l^e.
+def StrongApproximation(O0, N, C, D, l, max_cnt=10):
+    p = O0.discriminant()
+    e = floor(3 * log(N, l) + log(p, l))
+    Nrd_mu = p * (C**2 + D**2)
+    if (kronecker(Nrd_mu, N) == 1 and e % 2 == 1) or (kronecker(Nrd_mu, N) == -1 and e % 2 == 0):
+        e += 1
+
+    while True:
+        lam = ZZ(sqrt(GF(N)(l**e)/GF(N)(Nrd_mu)))
+        rhs = ZZ((l**e - lam**2 * Nrd_mu) / N)
+
+        for _ in range(max_cnt):
+            c = GF(N).random_element()
+            d = ZZ((GF(N)(rhs) / GF(N)(2*p*lam) - C*c) / GF(N)(D))
+            c = ZZ(c)
+
+            x = ZZ(-GF(N)(C)/GF(N)(D))
+            L = IntegerLattice([[N, N*x], [0, N**2]])
+            v = L.approximate_closest_vector([-lam*C - N*c, -lam*D - N*d])
+            Nc = N*c + v[0]
+            Nd = N*d + v[1]
+
+            tmp = ZZ((l**e - p*((lam*C + N*c)**2 + (lam*D + N*d)**2)) / N**2)
+            a, b = SumOf2Squares(tmp)
+            if a is not None and b is not None:
+                nu = O0([N*a, N*b, lam*C + N*c, lam*D + N*d])
+                assert nu.reduced_norm() == l**e
+                return nu
+        e += 2
 
 # return a left O0-ideal of norm N
 # Algorithm 3 in https://eprint.iacr.org/2024/760.pdf
--- test.sage
+++ test.sage
@@ -3,10 +3,11 @@ from quaternion import *
 p = 103
 B.<qi, qj, qk> = QuaternionAlgebra(QQ, -1, -p)
 O0 = B.maximal_order()
-N = 10007
+N = 2003
 
 I1, beta1 = RandomFixedNormIdeal(O0, N)
 I2, beta2 = RandomFixedNormIdeal(O0, N)
 
 C, D = IdealModConstraint(O0, qj, qk, beta1, beta2, N)
-
+nu = StrongApproximation(O0, N, C, D, 2)
+print("nu = %s, Nrd(nu) = %s" % (nu, factor(nu.reduced_norm())))
