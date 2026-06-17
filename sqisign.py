f934e776051ea998ceeea13392f6d660b0abde99
Onuki <onuki@mist.i.u-tokyo.ac.jp>
2026-06-17 18:27:48 +0900

    fix bug in torsion basis

--- sqisign.py
+++ sqisign.py
@@ -1,5 +1,6 @@
 from sage.all import (
     sqrt,
+    kronecker,
     EllipticCurve,
 )
 import special_curve
@@ -12,9 +13,7 @@ class SQIsign:
     @staticmethod
     def _deterministic_torsion_basis(E, e):
         """
-        Compute a deterministic basis of E[2^e] using Pari's implementation
-        of the Weil pairing. This is used to compute the action of the
-        quaternion order on E[2^e].
+        Compute a deterministic basis of E[2^e] for a supersingular Montgomery curve
         """
         F = E.base_ring()
         p = F.characteristic()
@@ -23,18 +22,23 @@ class SQIsign:
         assert A != 0
         assert E == EllipticCurve(F, [0, A, 0, 1, 0]) # y^2 = x^3 + A*x^2 + x
 
-        h = F(0)
-        xP = F(0)
+        h = 0
         if A.is_square():
-            print("A is square")
-            while (1 + h**2).is_square() or not (xP**3 + A*xP**2 + xP).is_square():
+            # We need the denominator 1 + i*h to be a non-square in F_{p^2},
+            # which holds iff its norm 1 + h^2 is a non-square in F_p.
+            while True:
                 h += 1
                 xP = -1/(1 + i*h)*A
+                if kronecker(1 + h**2, p) == 1:
+                    continue
+                if (xP**3 + A*xP**2 + xP).is_square():
+                    break
         else:
-            print("A is not square")
-            while not (xP**3 + A*xP**2 + xP).is_square():
+            while True:
                 h += 1
                 xP = h*A
+                if (xP**3 + A*xP**2 + xP).is_square():
+                    break
         cofactor = (p + 1) // (2**e)
         P = cofactor * E.lift_x(xP)
         Q = cofactor * E.lift_x(-xP - A)
