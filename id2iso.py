26f2a81d2c51e7db85edfad13abbc5ce9231155c
Onuki <onuki@mist.i.u-tokyo.ac.jp>
2026-06-17 17:13:57 +0900

    id2iso complete

--- id2iso.py
+++ id2iso.py
@@ -5,6 +5,7 @@ from sage.all import (
     gcd,
     ZZ,
     ceil,
+    inverse_mod,
 )
 from theta_structures.couple_point import CouplePoint
 from theta_isogenies.product_isogeny import EllipticProductIsogeny
@@ -81,31 +82,49 @@ def Qlapoti(I, e, max_tries=10000):
 def IdealToIsogeny(E0withEnd, I):
     E0 = E0withEnd.curve
     O = E0withEnd.order
+    N = norm(I)
     assert I.left_order() == O
+    assert N % 2 == 1
     e = E0withEnd.e
 
+    """
+    compute beta1, beta2, gamma s.t.
+    beta1 = hat{phi_I} * phi_1, beta2 = hat{phi_I} * phi_2,
+    gamma = hat{phi_2} * phi_1,
+    where phi_I is the isogeny corresponding to I,
+    deg(phi_1) + deg(phi_2) = 2^(e-2).
+    """
     beta1, beta2, gamma = Qlapoti(I, e - 2)
     assert beta1 in I and beta2 in I
-    assert beta2 * beta1.conjugate() / norm(I) == gamma
-    d1 = beta1.reduced_norm() / norm(I)
-    d2 = beta2.reduced_norm() / norm(I)
+    assert beta2 * beta1.conjugate() / N == gamma
+    d1 = beta1.reduced_norm() / N
+    d2 = beta2.reduced_norm() / N
     assert d1 + d2 == 2**(e - 2)
+    assert d1 % 2 == 1 and d2 % 2 == 1
 
+    # compute (2^(e-2), 2^(e-2))-isogeny
     P1, Q1 = d1 * E0withEnd.P, d1 * E0withEnd.Q
     P2, Q2 = E0withEnd.quaternion_action(gamma)
     K1 = CouplePoint(P1, P2)
     K2 = CouplePoint(Q1, Q2)
     Phi = EllipticProductIsogeny((K1, K2), e-2)
-    image1 = Phi(CouplePoint(E0withEnd.P, E0(0)))
-    image2 = Phi(CouplePoint(E0withEnd.Q, E0(0)))
-    image_sum = Phi(CouplePoint(E0withEnd.P + E0withEnd.Q, E0(0)))
+    P0, Q0 = E0withEnd.quaternion_action(beta1.conjugate()) # P0 = hat{phi_1}*phi_I(P), Q0 = hat{phi_1}*phi_I(Q)
+    image1 = Phi(CouplePoint(P0, E0(0)))
+    image2 = Phi(CouplePoint(Q0, E0(0)))
+    image_sum = Phi(CouplePoint(P0 + Q0, E0(0)))
 
+    # determine the codomain EI by comparing the Tate pairings
+    tP0Q0d1 = E0withEnd.tate_pairing_PQ**(d1**2*N)
     for idx in range(2):
         Pim, Qim, PQim = image1[idx], image2[idx], image_sum[idx]
-        if Pim + Qim != PQim:
+        if not (Pim + Qim == PQim or Pim + Qim == -PQim):
             Qim = -Qim
+        assert Pim + Qim == PQim or Pim + Qim == -PQim
         exp = (E0withEnd.p**2 - 1) / 2**e
         tPimQim = tate_pairing_pari(Pim, Qim, 2**e)**exp
-        if tPimQim == E0withEnd.tate_pairing_PQ**d1:
-            return Phi.codomain()[idx], Pim, Qim
-
+        if tPimQim == tP0Q0d1:
+            EI = Phi.codomain()[idx]
+            PI = inverse_mod(d1, 2**e) * Pim
+            QI = inverse_mod(d1, 2**e) * Qim
+            return EI, PI, QI
+    raise ValueError("Failed to determine the codomain of the isogeny")
